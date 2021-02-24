96fdf8acd7024e81d175428f83e49d26371c8301
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-02-24 21:18:18 +0100

    Added Locale Helpers

---
+++ src/Helpers/Locale.php
@@ -0,0 +1,19 @@
+<?php
+namespace AndreaMarelli\ModularForms\Helpers;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+
+class Locale{
+
+    public static function lower()
+    {
+        return mb_strtolower(App::getLocale() ?? Config::get('app.locale'));
+    }
+
+    public static function upper()
+    {
+        return mb_strtoupper(App::getLocale() ?? Config::get('app.locale'));
+    }
+
+}
