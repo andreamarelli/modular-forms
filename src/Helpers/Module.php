7742e56eff6c8963c26fd9e3f076ed2ae9ffcbee
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-02-26 00:12:02 +0100

    Added module helper

---
+++ src/Helpers/Module.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AndreaMarelli\ModularForms\Helpers;
+
+use Illuminate\Support\Facades\DB;
+
+
+class Module
+{
+
+    public static function getModulesList($forms)
+    {
+        $modules = [];
+        foreach ($forms as $form_modules) {
+            foreach ($form_modules as $module) {
+                if (is_array($module)) {
+                    foreach ($module as $moduleOfStep) {
+                        if (!in_array($moduleOfStep, $modules)) {
+                            $modules[] = $moduleOfStep;
+                        }
+                    }
+                } elseif (!in_array($module, $modules)) {
+                    $modules[] = $module;
+                }
+            }
+        }
+        return $modules;
+    }
+
+    /**
+     * Iterate over all modules of the given forms and apply callback function
+     *
+     * @param $forms
+     * @param $callback
+     * @param $callback_args
+     */
+    public static function iterateOverModules($forms, $callback, $callback_args)
+    {
+        $modules = self::getModulesList($forms);
+        foreach ($modules as $moduleClass) {
+            $args = $callback_args;
+            array_unshift($args, $moduleClass);
+            call_user_func($callback, $args);
+        }
+    }
+
+    /**
+     * Check if the given field in a module has records with the given value
+     *
+     * @param $moduleClass
+     * @param $field
+     * @param $value
+     * @return bool
+     */
+    public static function checkIfFieldHasValue($moduleClass, $field, $value)
+    {
+        $db_table = (new $moduleClass())->getTable();
+        $res      = DB::table($db_table)
+            ->select(DB::raw('count(*)'))
+            ->where($field, $value)
+            ->first();
+        return $res->count > 0;
+    }
+
+    /**
+     * Inject in blade-generated DOM the average calculation for the given GROUP
+     *
+     * @param $view
+     * @param $group
+     * @param $num_columns
+     * @param $average_column
+     * @param string $vModelName
+     * @return mixed
+     */
+    public static function injectAverageInGroup($view, $group, $num_columns, $average_column, $vModelName = 'averages')
+    {
+        $searchFor = '<tbody class="' . $group . '">';
+
+        $textToAdd = '';
+        for ($c = 1; $c <= $num_columns; $c++) {
+            if ($c == $average_column) {
+                $textToAdd .= '<td class="text-center">
+                                  <input type="text" disabled="disabled" v-model="' . $vModelName . '.' . $group . '" class="field-disabled input-number field-edit text-center"/>
+                               </td>';
+            } else {
+                $textToAdd .= '<td></td>';
+            }
+        }
+        $textToAdd = '<thead><tr>' . $textToAdd . '</tr></thead>';
+
+        return str_replace($searchFor, $textToAdd . $searchFor, $view);
+    }
+
+    /**
+     * Inject in blade-generated DOM an additional title for the given GROUPs
+     *
+     * @param $view
+     * @param $module_key
+     * @param $beforeGroup
+     * @param $title
+     * @return mixed
+     */
+    public static function injectGroupTitle($view, $module_key, $beforeGroup, $title)
+    {
+        $searchFor = '<h5 class="green group_title_' . $module_key . '_' . $beforeGroup . '">';
+        $textToAdd = '<h3>' . $title . '</h3>';
+        return str_replace($searchFor, $textToAdd . $searchFor, $view);
+    }
+
+}
