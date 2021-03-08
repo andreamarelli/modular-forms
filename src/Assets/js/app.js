c6395aeed390a580782ab888e739916d1565d697
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-03-08 14:37:02 +0100

    Adding assets: trying to import mixins

--- src/Assets/js/app.js
+++ src/Assets/js/app.js
@@ -1,3 +1,15 @@
 
 // Load other js files
 require('./module/controller.js');
+
+
+// Loading mixins
+import filter from './mixins-vue/filter.mixin';
+import sorter from './mixins-vue/sorter.mixin';
+import paginate from './mixins-vue/paginate.mixin';
+window.Mixins = {};
+window.MixinsVue = {
+    'filter' : filter,
+    'sorter' : sorter,
+    'paginate' : paginate,
+};
---
+++ src/Assets/js/mixins-vue/filter.mixin.js
@@ -0,0 +1,65 @@
+export default {
+
+    data (){
+        return {
+            initial: null,
+            searchKey: null,
+        }
+    },
+
+    methods: {
+
+        filterList: function(items){
+            items = this.filterByInitial(items);
+            return items;
+        },
+
+        filterByInitial: function (items) {
+            if(this.initial!==null){
+                items = items.filter((item) => {
+                    if(item.name.charAt(0).toLowerCase() === this.initial.toLowerCase()){
+                        return true;
+                    }
+                });
+            }
+            return items;
+        },
+
+        filterByAttribute: function (items, filter_value, filter_on){
+            let _this = this;
+            filter_value = filter_value==="" || filter_value==='null' ? null : filter_value;
+            if(filter_value!==null){
+                items = items.filter((item) => {
+                    let value = _this.__getAttribute(item, filter_on);
+                    if(value.toString().toLowerCase() === filter_value.toString().toLowerCase()){
+                        return true;
+                    }
+                });
+            }
+            return items;
+        },
+
+        __getAttribute: function (item, attribute){
+            let value = null;
+
+            /* More than 1 level deep */
+            if(attribute.includes('.')){
+                let path = attribute.split('.');
+                value = item;
+                for (let i = 0; i < path.length; ++i) {
+                    value = value[path[i]];
+                }
+            }
+            /* simple attribute */
+            else {
+                value = item[attribute];
+            }
+
+            value = value===null ? '' : value;
+
+            return value;
+        }
+
+    }
+
+}
