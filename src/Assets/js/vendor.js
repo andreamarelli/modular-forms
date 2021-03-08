81e88322095b07d01d9e5f3787faaf9dc678165f
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-03-08 09:53:51 +0100

    Adding assets

---
+++ src/Assets/js/vendor.js
@@ -0,0 +1,12 @@
+
+window._ = require('lodash');
+window.axios = require('axios');
+window.$ = window.jQuery = require('jquery');
+
+// Vue
+window.Vue = require('vue');
+window.vueBus = new Vue();
+
+// Vuex
+window.Vuex = require('vuex');
+window.Vue.use(window.Vuex);
