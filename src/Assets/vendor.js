2eef011518401b65366556b3d98c4bc502a078f3
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-07-14 18:01:18 +0200

    Moved bootstrap in

--- src/Assets/vendor.js
+++ src/Assets/vendor.js
@@ -11,6 +11,14 @@ window.vueBus = new window.Vue();
 window.Vuex = require('vuex');
 window.Vue.use(window.Vuex);
 
+// Bootstrap + related packages
+require('bootstrap');
+require('bootstrap-select');
+require('bootstrap-datepicker');
+require('select2');
+require('select2/dist/js/i18n/fr');
+window.$.fn.select2.defaults.set("theme", "bootstrap");
+
 // Other packages
 window.AutoNumeric = require('autonumeric');
 window.VueSelect = require('vue-select');
