cd515a6bf1925cb3ed300e1ca043e1af614a64e8
Andrea Marelli <andrea.marelli.dev@gmail.com>
2022-02-09 16:00:08 +0100

    Fixed path to label

--- src/Views/tables/validation.blade.php
+++ src/Views/tables/validation.blade.php
@@ -1,4 +1,4 @@
 <div v-if="item.valid===false"
-           data-toggle="tooltip" data-placement="top" data-original-title="@lang_u('modular-forms::common.form..error')">
+           data-toggle="tooltip" data-placement="top" data-original-title="@lang_u('modular-forms::common.form.error')">
     <i class="fas fa-exclamation-triangle contextual_danger fa-2x"></i>
 </div>
