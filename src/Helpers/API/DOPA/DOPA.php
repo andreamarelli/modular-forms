181d4b9b70329862beee75e344970bed1df48049
Andrea Marelli <andrea.marelli.dev@gmail.com>
2022-05-12 16:00:17 +0200

    Fixed introduced issue

--- src/Helpers/API/DOPA/DOPA.php
+++ src/Helpers/API/DOPA/DOPA.php
@@ -39,7 +39,7 @@ class DOPA
                 return $response['metadata']['error'];
             }
         } else {
-            return $response['error'];
+            return $response['record'];
         }
         return $response;
     }
