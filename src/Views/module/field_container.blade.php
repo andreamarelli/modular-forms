60d5c5abe4e62e0027a7893661f36aa26435fd20
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-02-16 10:37:46 +0100

    Custom attributes in field_container

--- src/Views/module/field_container.blade.php
+++ src/Views/module/field_container.blade.php
@@ -2,15 +2,19 @@
 /** @var String     $name */
 /** @var String     $label */
 /** @var int    $label_width */
+/** @var String   $other_attributes */
 
 $label_width = $label_width ?? 3;
 
 $style_width = $label_width!==3
     ? 'style="width: '.round(100/12*$label_width).'%;"'      // bootstrap col-lg-x to %
     : '';
+
+$other_attributes = $other_attributes ?? '';
+
 ?>
 
-<div class="module-row">
+<div class="module-row" {!! $other_attributes !!}}>
 
     {{-- label  --}}
     <div class="module-row__label" {!! $style_width !!}>
