184ebf7fef3666b7523d90c2b507bf4a4b09a9a1
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-12-21 17:04:35 +0100

    Fixed issue with tooltip on click

--- src/resources/assets/js/templates/tooltip.vue
+++ src/resources/assets/js/templates/tooltip.vue
@@ -90,15 +90,15 @@ export default {
             let _this = this;
             // toggle on anchor click
             this.anchorElem.addEventListener('click', this.isVisible ? this.hideTooltip : this.showTooltip);
-            if(this.isVisible){
-                // close on click outside tooltip
-                document.addEventListener('click', function(evt){
+            // close on click outside tooltip
+            document.addEventListener('click', function(evt){
+                if(_this.isVisible) {
                     let clickedElem = evt.target;
-                    if(clickedElem.closest('.tooltip-content') == null){
+                    if (clickedElem.closest('.tooltip-content') == null) {
                         _this.hideTooltip();
                     }
-                });
-            }
+                }
+            });
         },
 
         /**
