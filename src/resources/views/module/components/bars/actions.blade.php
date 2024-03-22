71dd2b57c76d90558df6c86145fbdfadd5e0c54a
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-03-22 10:46:10 +0100

    View renamed

--- src/resources/views/module/components/bars/actions.blade.php
+++ src/resources/views/module/components/bars/actions.blade.php
@@ -1,4 +1,3 @@
-
 @if($mode === \AndreaMarelli\ModularForms\View\Module\Container::MODE_EDIT)
     @include('modular-forms::module.components.bars.save', compact(['controller', 'definitions', 'records', 'mode']))
 @endif
