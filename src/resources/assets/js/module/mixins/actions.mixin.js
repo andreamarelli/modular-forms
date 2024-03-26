3b5760913119858c71ce9ec80385fa2cddf007ae
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-03-26 14:40:55 +0100

    Fixed NotApplicable/NotAvailable with GROUP types

--- src/resources/assets/js/module/mixins/actions.mixin.js
+++ src/resources/assets/js/module/mixins/actions.mixin.js
@@ -13,32 +13,56 @@ export default {
             if(this.enable_not_applicable===true
                 && 'not_applicable' in this.records[0]
             ){
-                this.not_applicable = this.records[0]['not_applicable']===true;
-                this.not_available = this.records[0]['not_available']===true;
+                if(this.module_type.includes('GROUP_')){
+                    let first_group_key = Object.keys(this.groups)[0];
+                    this.not_applicable = this.records[first_group_key][0]['not_applicable']===true;
+                    this.not_available = this.records[first_group_key][0]['not_available']===true;
+                } else {
+                    this.not_applicable = this.records[0]['not_applicable']===true;
+                    this.not_available = this.records[0]['not_available']===true;
+                }
             }
         },
 
         toggleNotApplicable: function () {
             let _this = this;
-            _this.not_applicable = !_this.not_applicable;
-            _this.not_available = false;
-            _this.records = [];
-            _this.records.push(_this.__no_reactive_copy(_this.empty_record));
-            this.common_fields.forEach(function (field) {
-                _this.records[0][field['name']] = null;
-            });
-            _this.records[0]['not_applicable'] = _this.not_applicable === true ? true : null;
+            this.not_applicable = !this.not_applicable;
+            this.not_available = false;
+            this.records = [];
+            this.records.push(this.__no_reactive_copy(this.empty_record));
+            this.__arrange_records_by_group();
+            if(this.module_type.includes('GROUP_')){
+                let first_group_key = Object.keys(this.groups)[0];
+                this.common_fields.forEach(function (field) {
+                    _this.records[first_group_key][0][field['name']] = null;
+                });
+                this.records[first_group_key][0]['not_applicable'] = this.not_applicable === true ? true : null;
+            } else {
+                this.common_fields.forEach(function (field) {
+                    _this.records[0][field['name']] = null;
+                });
+                this.records[0]['not_applicable'] = this.not_applicable === true ? true : null;
+            }
         },
 
         toggleNotAvailable: function () {
             let _this = this;
-            _this.not_available = !_this.not_available;
-            _this.records = [];
-            _this.records.push(_this.__no_reactive_copy(_this.empty_record));
-            this.common_fields.forEach(function (field) {
-                _this.records[0][field['name']] = null;
-            });
-            _this.records[0]['not_available'] = _this.not_available === true ? true : null;
+            this.not_available = !this.not_available;
+            this.records = [];
+            this.records.push(this.__no_reactive_copy(this.empty_record));
+            this.__arrange_records_by_group();
+            if(this.module_type.includes('GROUP_')){
+                let first_group_key = Object.keys(this.groups)[0];
+                this.common_fields.forEach(function (field) {
+                    _this.records[first_group_key][0][field['name']] = null;
+                });
+                this.records[first_group_key][0]['not_available'] = this.not_available === true ? true : null;
+            } else {
+                this.common_fields.forEach(function (field) {
+                    _this.records[0][field['name']] = null;
+                });
+                this.records[0]['not_available'] = this.not_available === true ? true : null;
+            }
         }
 
     }
