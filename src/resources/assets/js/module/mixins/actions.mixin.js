686186ccb4d95c920dfed847c465908ea84826bf
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-03-26 17:20:07 +0100

    Fixing issue with NotApplicable/NotAvailable in GROUP types

--- src/resources/assets/js/module/mixins/actions.mixin.js
+++ src/resources/assets/js/module/mixins/actions.mixin.js
@@ -27,44 +27,44 @@ export default {
         },
 
         toggleNotApplicable: function () {
-            let _this = this;
             this.not_applicable = !this.not_applicable;
             this.not_available = false;
-            this.records = [];
-            this.records.push(this.__no_reactive_copy(this.empty_record));
+            let records = [];
+            records.push(this.__no_reactive_copy(this.empty_record));
             this.__arrange_records_by_group();
             if(this.module_type.includes('GROUP_')){
                 let first_group_key = Object.keys(this.groups)[0];
                 this.common_fields.forEach(function (field) {
-                    _this.records[first_group_key][0][field['name']] = null;
+                    records[first_group_key][0][field['name']] = null;
                 });
-                this.records[first_group_key][0]['not_applicable'] = this.not_applicable === true ? true : null;
+                records[first_group_key][0]['not_applicable'] = this.not_applicable === true ? true : null;
             } else {
                 this.common_fields.forEach(function (field) {
-                    _this.records[0][field['name']] = null;
+                    records[0][field['name']] = null;
                 });
-                this.records[0]['not_applicable'] = this.not_applicable === true ? true : null;
+                records[0]['not_applicable'] = this.not_applicable === true ? true : null;
             }
+            this.records = records;
         },
 
         toggleNotAvailable: function () {
-            let _this = this;
             this.not_available = !this.not_available;
-            this.records = [];
-            this.records.push(this.__no_reactive_copy(this.empty_record));
+            let records = [];
+            records.push(this.__no_reactive_copy(this.empty_record));
             this.__arrange_records_by_group();
             if(this.module_type.includes('GROUP_')){
                 let first_group_key = Object.keys(this.groups)[0];
                 this.common_fields.forEach(function (field) {
-                    _this.records[first_group_key][0][field['name']] = null;
+                    records[first_group_key][0][field['name']] = null;
                 });
-                this.records[first_group_key][0]['not_available'] = this.not_available === true ? true : null;
+                records[first_group_key][0]['not_available'] = this.not_available === true ? true : null;
             } else {
                 this.common_fields.forEach(function (field) {
-                    _this.records[0][field['name']] = null;
+                    records[0][field['name']] = null;
                 });
-                this.records[0]['not_available'] = this.not_available === true ? true : null;
+                records[0]['not_available'] = this.not_available === true ? true : null;
             }
+            this.records = records;
         }
 
     }
