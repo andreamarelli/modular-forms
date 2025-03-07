69111b225eca094f373d7c2b385d49111c29b624
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-03-07 09:54:28 +0100

    Removed unused js dependencies

--- src/resources/assets/js/helpers/animation.js
+++ src/resources/assets/js/helpers/animation.js
@@ -10,28 +10,6 @@ export default {
 
     scrollPageToAnchor(anchor) {
         this.scrollPageTo(document.querySelector('#'+anchor).offsetTop);
-    },
-
-    copyToClipboard(textToCopy, alertMessage) {
-        let dummy = document.createElement("textarea");
-        document.body.appendChild(dummy);
-        dummy.value = textToCopy;
-        dummy.select();
-        document.execCommand("copy");
-        document.body.removeChild(dummy);
-        this.showAlert(alertMessage);
-    },
-
-    showAlert(message){
-        let element = document.createElement("div");
-        // element.setAttribute("id", "tempAlert");
-        element.style.display = 'none';
-        element.className = 'alert-popup';
-        element.innerHTML = message;
-        document.body.appendChild(element);
-        $(element).fadeTo(2000, 500).slideUp(500, function () {
-            document.body.removeChild(element);
-        });
     }
 
 }
