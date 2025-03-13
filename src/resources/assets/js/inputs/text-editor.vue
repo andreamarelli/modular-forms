abab1b9218a08999d95c97c0741377b6784329bd
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-03-13 10:28:55 +0100

    Implemented Quill editor

--- src/resources/assets/js/inputs/text-editor.vue
+++ src/resources/assets/js/inputs/text-editor.vue
@@ -1,13 +1,59 @@
 <template>
 
-    <div ref="textEditorComponent"></div>
+    <div class="container">
+        <div class="field-edit" ref="textEditorComponent" v-html="editorValue"></div>
+    </div>
 
 </template>
 
+<style scoped>
+.container, .ql-container{
+    max-width: 800px;
+}
+
+</style>
+<style>
+.ql-toolbar{
+    background: white;
+}
+</style>
+
 <script setup>
-import { onMounted, onUnmounted, ref, watch } from 'vue';
+import { onMounted, ref, unref } from 'vue';
+import Quill from 'quill';
+import Bold from 'quill/formats/bold';
+import Italic from 'quill/formats/italic';
+import Link from 'quill/formats/link';
+import List from 'quill/formats/list';
+import Header from 'quill/formats/header';
+
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+
 
 const inputValue = defineModel();
+const editorValue = ref(unref(inputValue));
 const textEditorComponent = ref(null);
+
+let quill = null;
+const options = {
+    modules: {
+        toolbar: [
+            ['bold', 'italic'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            ['link'],
+            [{ header: [2, false] }],
+        ],
+    },
+    theme: 'snow'
+};
+
+onMounted(() => {
+    quill = new Quill(textEditorComponent.value, options);
+    quill.on('text-change', () => {
+        inputValue.value = quill.container.querySelector('.ql-editor').innerHTML;
+    });
+});
+
 
 </script>
